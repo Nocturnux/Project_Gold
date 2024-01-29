291b577bedbe27e5dcd0a84c0baac553833b9d95
Camilaaj18 <camilaalvarez856@gmail.com>
2024-01-29 09:51:54 -0500

    update_booking_service

--- Glamping_Gold/booking_service/urls.py
+++ Glamping_Gold/booking_service/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [      
-    path('', views.booking_service, name='booking_service'),            
+    path('', views.booking_service, name='booking_service'),   
+    path('booking_service_status_/<int:booking_service_id>/', views.change_status_booking_service, name='booking_service_status'),         
 ]
