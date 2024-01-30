683c9ed6fd84df3e12fa35b7f51436ebaffd0ef5
Sebastian0305 <xavier81165@gmail.com>
2024-01-30 11:23:09 -0500

    formularioPayment

--- Glamping_Gold/payment/views.py
+++ Glamping_Gold/payment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-
 from payment.models import Payment
+from .forms import PaymentForm
 
 def payment(request):    
     payment_list = Payment.objects.all()    
@@ -11,3 +11,10 @@ def change_status_payment(request, payment_id):
     payment.status = not payment.status
     payment.save()
     return redirect('payment')
+
+def create_payment(request):
+    form = PaymentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('payment')    
+    return render(request, 'payment/create.html', {'form': form})
