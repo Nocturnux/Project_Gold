dcac8d18e87352dfba199747e806a81b8ace5185
Camilaaj18 <camilaalvarez856@gmail.com>
2024-02-02 02:26:57 -0500

    btn delete cabin, cabin_type

--- Glamping_Gold/cabin_type/urls.py
+++ Glamping_Gold/cabin_type/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [      
     path('', views.cabin_type, name='cabin_type'),   
     path('cabin_type_status_/<int:cabin_type_id>/', views.change_status_cabin_type, name='cabin_type_status'),
-    path('create/', views.create_cabin_type, name='create_cabin_type'),         
+    path('create/', views.create_cabin_type, name='create_cabin_type'), 
+    path('detail/<int:cabin_type_id>/', views.detail_cabin_type, name='detail_cabin_type'),  
+    path('delete/<int:cabin_type_id>/', views.delete_cabin_type, name='delete_cabin_type'),         
 ]
