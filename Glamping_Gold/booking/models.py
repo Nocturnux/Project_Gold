800a8cb066df8abf805b8c4c0070939d0a4c2318
Nocturnux <genjuro128@yahoo.es>
2024-02-04 19:11:23 -0500

    ModelB

--- Glamping_Gold/booking/models.py
+++ Glamping_Gold/booking/models.py
@@ -6,7 +6,7 @@ class Booking(models.Model):
     date_end = models.DateField()
     value = models.IntegerField()
     state_booking = models.CharField(max_length=25)
-    id_customer = models.ForeignKey('customer.Customer', on_delete=models.DO_NOTHING)
+    customer = models.ForeignKey('customer.Customer', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.date_booking
