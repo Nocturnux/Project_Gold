c8596e8a0133d1f21b80cd6370f962d7c2c69f08
SergioEsmeral2004 <yeyoesmeral@hotmail.com>
2024-02-06 10:37:52 -0500

    editPayment

--- Glamping_Gold/payment/forms.py
+++ Glamping_Gold/payment/forms.py
@@ -5,15 +5,17 @@ class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = "__all__"
-        exclude = ['status','id_booking','state_payment']
+        exclude = ['state_payment']
         labels = {
             'date': 'Fecha',
             'payment_method': 'Metodo de pago',
-            'value': 'Valor',  
+            'value': 'Valor', 
+            'id_booking': 'Id reserva', 
                               
         }
         widgets = {
-            'date': forms.DateInput(attrs={'placeholder': 'Ingresa la fecha'}),
+            'date' : forms.DateInput(attrs={'type':'date'}),
             'payment_method': forms.TextInput(attrs={'placeholder': 'Ingresa el metodo de pago'}),
-            'value': forms.IntegerField(),                 
+            'value': forms.TextInput(attrs={'placeholder': 'Ingresa el valor'}),  
+            'id_booking' : forms.SelectMultiple(attrs={"placeholder":"Seleccione reserva"})               
         }
--- Glamping_Gold/payment/urls.py
+++ Glamping_Gold/payment/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', views.create_payment, name='create_payment'),    
     path('detail/<int:payment_id>/', views.detail_payment, name='detail_payment'),
     path('delete/<int:payment_id>/', views.delete_payment, name='delete_payment'),
+    path('edit/<int:payment_id>/', views.edit_payment, name='edit_payment'), 
 ]
